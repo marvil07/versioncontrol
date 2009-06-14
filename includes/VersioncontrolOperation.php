55d8590d3465e3e1ede6c36f72401aecc787fca4
Marco Antonio Villegas Vega <marvil07@gmail.com>
2009-06-14 13:35:19 -0500

    operation: arrayaccess simple implementation

--- includes/VersioncontrolOperation.php
+++ includes/VersioncontrolOperation.php
@@ -1205,6 +1205,20 @@ class VersioncontrolOperation {
     return $statistics;
   }
 
+  //ArrayAccess interface implementation
+  public function offsetExists($offset) {
+    return isset($this->$offset);
+  }
+  public function offsetGet($offset) {
+    return $this->$offset;
+  }
+  public function offsetSet($offset, $value) {
+    $this->$offset = $value;
+  }
+  public function offsetUnset($offset) {
+    unset($this->$offset);
+  }
+
 }
 
 ?>
